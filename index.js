53245c704509bcffb130c0f2e36e7164eda87a4a
Vandinho <evandro.menshaid@gmail.com>
2026-05-09 18:51:13 -0300

    N03 - Criando um Servidor Web com NodeJS

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Instalando o NodeJS');
+const http = require('http');
+
+let server = http.createServer((req, res) => {
+
+    console.log('URL', req.url);
+    console.log('METHOD:', req.method);
+
+    res.end('Ok');
+
+});
+
+server.listen(3000, '127.0.0.1', ()=> {
+
+    console.log('servidor rodando');
+
+});
