234cbbc1bd2294a1fb12ec50aefd98417435be22
Vandinho <evandro.menshaid@gmail.com>
2026-05-10 20:26:23 -0300

    N07 - Separando Rotas do Arquivo Principal

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 const express = require('express');
+let routesIndex = require('./routes/index');
+let routesUsers = require('.routes/users');
 
 let app = express();
 
-app.get('/', (req, res) => {
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Olá</h1>');
-
-});
-
-app.get('/users', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-        users: [{
-            name: 'Hcode',
-            email: 'contato@hcode.com',
-            id: 1
-        }]
-    });
-
-
-});
+app.use(routesIndex);
+app.use('/users', routesUsers);
 
 app.listen(3000, '127.0.0.1', () => {
 
