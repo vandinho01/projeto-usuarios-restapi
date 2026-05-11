6db56931b77684738f541760480192538278b619
Vandinho <evandro.menshaid@gmail.com>
2026-05-10 21:39:03 -0300

    N12 - Refatorando Sistema e Otimizando o código

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-consign().include('routes').into(app);
+consign().include('routes').incule('utils').into(app);
 
 app.listen(3000, '127.0.0.1', () => {
 
--- routes/users.js
+++ routes/users.js
@@ -5,15 +5,16 @@ let db = new NeDB({
 })
 
 module.exports = (app) => {
-    app.get('/users', (req, res) => {
+
+    let route = app.route('/users')
+
+    app.get((req, res) => {
 
         db.find({}).sort({ name: 1 }).exec((err, users) => {
 
             if (err) {
-                console.log(`Error: ${err}`);
-                res.status(400).json({
-                    error: err
-                });
+                app.utils.error.send(err, req, res);
+
             } else {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
@@ -26,16 +27,13 @@ module.exports = (app) => {
 
     });
 
-    app.post('/users', (req, res) => {
+    route.post((req, res) => {
         console.log(req.body);
 
         db.insert(req.body, (err, user) => {
 
             if (err) {
-                console.log(`Erros: ${err}`);
-                res.status(400).json({
-                    error: err
-                });
+                app.utils.error.send(err, req, res);
             } else {
                 res.status(200).json(user);
             }
