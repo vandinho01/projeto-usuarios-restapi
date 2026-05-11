7debf9e741a9d0b9bc66b973abe4f7961c4debf9
Vandinho <evandro.menshaid@gmail.com>
2026-05-10 21:55:31 -0300

    N15 - Excluindo um usuário

--- routes/users.js
+++ routes/users.js
@@ -68,4 +68,18 @@ module.exports = (app) => {
 
     });
 
+    routeId.delete((req, res) => {
+
+        db.remove({ _id: req.paramns.id }, {}, err => {
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json(req.paramns);
+            }
+
+        })
+
+    })
+
 };
